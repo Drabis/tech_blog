5aff21ad5ec467bfcae894666229a8b64fa67ffd
Drabis <drabis47@gmail.com>
2021-05-09 12:24:30 -0400

    add to js files

---
+++ public/js/deleteComment.js
@@ -0,0 +1,16 @@
+const deleteComment = async (element) => {
+
+  console.log("hi delete comment");
+  const comment_id = element.value;
+  console.log(comment_id);
+
+  const response = await fetch(`/api/comments/${comment_id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert("Failed to delete comment.");
+}
+};
