633eb7c08423911b459d3cc06d01c2fbed7b22e4
Drabis <drabis47@gmail.com>
2021-05-09 10:56:29 -0400

    added comment model, blogRoutes and dashroute

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const blogRoutes = require("./blogRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
+
+router.use("/blogs", blogRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
